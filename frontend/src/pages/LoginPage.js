4a327a4c11e000bc2c85d7b2f35b19f8f20904da
Manoj M <connectwithmanojm@gmail.com>
2025-06-09 15:33:37 +0530

    Create LoginPage.js

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { FaUser, FaLock } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import '../styles/LoginPage.css';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Check credentials
+        if (username === 'admin' && password === 'ECO!@#') {
+            localStorage.setItem('userToken', 'authenticated');
+            navigate('/home'); // Redirect to home page on success
+        } else {
+            alert('Invalid username or password');
+        }
+    };
+
+    return (
+
+    
+        <div className="login-container">
+            <h1 className="title">ASTERISK</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label>Username</label>
+                    <div className="input-wrapper">
+                        <FaUser className="input-icon" />
+                        <input
+                            className='username-field'
+                            type="text"
+                            placeholder="Enter your username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
+
+                <div className="input-group">
+                    <label>Password</label>
+                    <div className="input-wrapper">
+                        <FaLock className="input-icon" />
+                        <input
+                            className='password-field'
+                            type="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                </div>
+
+                <button type="submit" className="login-btn">
+                    Authorize
+                </button>
+            </form>
+        </div>
+
+    );
+}
+
+export default Login;
